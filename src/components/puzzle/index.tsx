272869ebcf47bd800cb7337099576e43fc30b828
Arissa Fram <arissafram@gmail.com>
2025-07-21 16:01:19 -0400

    init initial component

---
+++ src/components/puzzle/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.scss';
+
+const Puzzle: React.FC = () => {
+  return (
+    <div className="puzzle-root">
+      <h2>Puzzle Component</h2>
+      {/* Puzzle content will go here */}
+    </div>
+  );
+};
+
+export default Puzzle;
