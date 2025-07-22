982ebb91a4634a915bb6a25cc6702bf410f4fe73
Arissa Fram <arissafram@gmail.com>
2025-07-21 23:13:50 -0400

    update merge options

--- src/components/puzzle/index.tsx
+++ src/components/puzzle/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import styles from './styles.module.scss';
 import Board from '../board';
 import { PuzzleProvider } from '../../contexts/puzzle-context';
 import { PuzzleOptions, InitialPuzzleOptions } from '../../types';
 import { DEFAULT_PUZZLE_OPTIONS } from '@/constants';
+import { mergeOptions } from './helpers/merge-options';
+
+import styles from './styles.module.scss';
 
 interface PuzzleProps {
   image: string;
@@ -15,35 +16,8 @@ interface PuzzleContentProps {
   options: PuzzleOptions;
 }
 
-// Type-safe deep merge utility
-function mergeOptions(
-  defaults: typeof DEFAULT_PUZZLE_OPTIONS,
-  options?: InitialPuzzleOptions,
-): typeof DEFAULT_PUZZLE_OPTIONS {
-  if (!options) return defaults;
-
-  return {
-    board: {
-      className: options.board?.className ?? defaults.board.className,
-      columns: options.board?.columns ?? defaults.board.columns,
-      height: options.board?.height ?? defaults.board.height,
-      rows: options.board?.rows ?? defaults.board.rows,
-      showGridOutlines: options.board?.showGridOutlines ?? defaults.board.showGridOutlines,
-      width: options.board?.width ?? defaults.board.width,
-    },
-    puzzlePiece: {
-      strokeColor: options.puzzlePiece?.strokeColor ?? defaults.puzzlePiece.strokeColor,
-      strokeEnabled: options.puzzlePiece?.strokeEnabled ?? defaults.puzzlePiece.strokeEnabled,
-      strokeWidth: options.puzzlePiece?.strokeWidth ?? defaults.puzzlePiece.strokeWidth,
-    },
-    shuffleArea: options.shuffleArea ?? defaults.shuffleArea,
-  };
-}
-
 const PuzzleContent: React.FC<PuzzleContentProps> = (props: PuzzleContentProps) => {
   const { image, options } = props;
-
-  // const mergedOptions = mergeOptions(DEFAULT_PUZZLE_OPTIONS, options);
 
   return (
     <div className={styles.puzzle}>
