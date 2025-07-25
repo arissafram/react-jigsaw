990c2df3fa1209f9e5ae19e63397c47328c06222
Arissa Fram <arissafram@gmail.com>
2025-07-24 20:19:01 -0400

    move puzzle-content into separate component

---
+++ src/components/puzzle/components/puzzle-content/index.tsx
@@ -0,0 +1,71 @@
+import Board from '@/components/board';
+import Settings from '@/components/settings';
+import { usePuzzleContext } from '@/contexts/puzzle-context';
+import { PuzzleOptions } from '@/types';
+
+import styles from './styles.module.scss';
+
+interface PuzzleContentProps {
+  image: string;
+  options: PuzzleOptions;
+  onComplete?: () => void;
+}
+
+const PuzzleContent: React.FC<PuzzleContentProps> = (props: PuzzleContentProps) => {
+  const { image, options, onComplete } = props;
+  const {
+    rows,
+    columns,
+    setBoardGrid,
+    refreshBoard,
+    timerIsRunning,
+    refreshCount,
+    setTimerIsRunning,
+  } = usePuzzleContext();
+
+  const handleBoardSlotChange = (newRows: number, newColumns: number) => {
+    setBoardGrid(newRows, newColumns);
+  };
+
+  const handleRefresh = () => {
+    refreshBoard();
+  };
+
+  const handlePuzzleComplete = () => {
+    setTimerIsRunning(false);
+    onComplete?.();
+  };
+
+  return (
+    <div
+      className={styles.puzzle}
+      style={{
+        width: `${options.board.width}px`,
+        minHeight: `${options.board.height}px`,
+      }}
+    >
+      <Board
+        key={`${rows}-${columns}-${refreshCount}`}
+        boardHeight={options.board.height}
+        boardWidth={options.board.width}
+        className={options.board.className}
+        columns={columns}
+        image={image}
+        puzzlePieceOptions={options.puzzlePiece}
+        rows={rows}
+        showBoardSlotOutlines={options.board.showBoardSlotOutlines}
+        onPuzzleComplete={handlePuzzleComplete}
+      />
+      <Settings
+        currentRows={rows}
+        currentColumns={columns}
+        onBoardSlotChange={handleBoardSlotChange}
+        onRefresh={handleRefresh}
+        timerIsRunning={timerIsRunning}
+        settings={options.puzzle.settings}
+      />
+    </div>
+  );
+};
+
+export default PuzzleContent;
--- src/components/puzzle/index.tsx
+++ src/components/puzzle/index.tsx
@@ -1,82 +1,16 @@
-import { useState } from 'react';
-import Board from '@/components/board';
-import Settings from '@/components/settings';
+import { PuzzleProvider } from '@/contexts/puzzle-context';
 import { DEFAULT_PUZZLE_OPTIONS } from '@/constants';
-import { PuzzleProvider, usePuzzleContext } from '@/contexts/puzzle-context';
 import { PuzzleOptions, InitialPuzzleOptions } from '@/types';
 
 import { mergeOptions } from './helpers/merge-options';
 
-import styles from './styles.module.scss';
+import PuzzleContent from './components/puzzle-content';
 
 interface PuzzleProps {
   image: string;
   options?: InitialPuzzleOptions;
   onComplete?: () => void;
 }
-
-interface PuzzleContentProps {
-  image: string;
-  options: PuzzleOptions;
-  onComplete?: () => void;
-}
-
-const PuzzleContent: React.FC<PuzzleContentProps> = (props: PuzzleContentProps) => {
-  const { image, options, onComplete } = props;
-  const {
-    rows,
-    columns,
-    setBoardGrid,
-    refreshBoard,
-    timerIsRunning,
-    refreshCount,
-    setTimerIsRunning,
-  } = usePuzzleContext();
-
-  const handleBoardSlotChange = (newRows: number, newColumns: number) => {
-    setBoardGrid(newRows, newColumns);
-  };
-
-  const handleRefresh = () => {
-    refreshBoard();
-  };
-
-  const handlePuzzleComplete = () => {
-    setTimerIsRunning(false);
-    onComplete?.();
-  };
-
-  return (
-    <div
-      className={styles.puzzle}
-      style={{
-        width: `${options.board.width}px`,
-        minHeight: `${options.board.height}px`,
-      }}
-    >
-      <Board
-        key={`${rows}-${columns}-${refreshCount}`}
-        boardHeight={options.board.height}
-        boardWidth={options.board.width}
-        className={options.board.className}
-        columns={columns}
-        image={image}
-        puzzlePieceOptions={options.puzzlePiece}
-        rows={rows}
-        showBoardSlotOutlines={options.board.showBoardSlotOutlines}
-        onPuzzleComplete={handlePuzzleComplete}
-      />
-      <Settings
-        currentRows={rows}
-        currentColumns={columns}
-        onBoardSlotChange={handleBoardSlotChange}
-        onRefresh={handleRefresh}
-        timerIsRunning={timerIsRunning}
-        settings={options.puzzle.settings}
-      />
-    </div>
-  );
-};
 
 const Puzzle: React.FC<PuzzleProps> = (props: PuzzleProps) => {
   const { options } = props;
