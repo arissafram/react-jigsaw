568e7c030275e2b6e45ad5b8fb5379a2c197589a
Arissa Fram <arissafram@gmail.com>
2025-07-31 20:30:51 -0400

    update demoprops

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -18,10 +18,10 @@ const App = () => {
     <Puzzle
       image={imageSource}
       onRefresh={handleRefresh}
-      responsive={true}
       options={{
         board: { columns: 2, rows: 2 },
         puzzle: {
+          responsive: true,
           settings: {
             enabled: true,
             timer: { enabled: true },
