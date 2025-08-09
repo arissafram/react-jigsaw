d8baaa7e2ab2e86fdba73ef1d4db9aacb693bb3a
Arissa Fram <arissafram@gmail.com>
2025-08-09 18:15:04 -0400

    Add TypeScript declaration files to dist folder
    - Generated .d.ts files for all components and types
    - Fixes TypeScript import errors in consuming projects
    - Includes proper type exports for Puzzle component

---
+++ dist/index.d.ts
@@ -0,0 +1,4 @@
+export { default as Puzzle } from './components/puzzle';
+export type { InitialPuzzleOptions, PuzzleOptions } from './types';
+export { DEFAULT_PUZZLE_OPTIONS } from './constants';
+//# sourceMappingURL=index.d.ts.map
