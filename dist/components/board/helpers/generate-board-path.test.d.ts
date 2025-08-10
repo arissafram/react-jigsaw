3240b7cc87d3a33bfe30ef31339b75686607ea2b
Arissa Fram <31447108+arissafram@users.noreply.github.com>
2025-08-10 13:10:19 -0400

    Rebuild app with mobile updates (#34)

--- dist/components/board/helpers/generate-board-path.test.d.ts
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=generate-board-path.test.d.ts.map
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as Puzzle } from './components/puzzle';
-export type { InitialPuzzleOptions, PuzzleOptions } from './types';
-export { DEFAULT_PUZZLE_OPTIONS } from './constants';
-//# sourceMappingURL=index.d.ts.map
