426acd7fc876856df9a097f1d11918b2df754a31
Arissa Fram <31447108+arissafram@users.noreply.github.com>
2025-08-10 23:48:29 -0400

    Update images (#44)
    - add Dasie images
    - prevent zoom in on inputs

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -6,20 +6,24 @@ import PropOptionsWrapper from './components/props-options-wrapper';
 
 import './styles.scss';
 
-const DEMO_IMAGE =
-  'https://images.unsplash.com/photo-1611003228941-98852ba62227?q=80&w=2148&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+import dasie1 from './assets/dasie-1.jpg';
+import dasie2 from './assets/dasie-2.jpg';
+import dasie3 from './assets/dasie-3.jpg';
+import dasie4 from './assets/dasie-4.jpg';
 
-const DEMO_IMAGE_2 =
-  'https://cdn.outsideonline.com/wp-content/uploads/2023/03/Funny_Dog_H.jpg?auto=webp&width=3840&quality=75&fit=cover';
+const DASIE_IMAGES = [dasie1, dasie2, dasie3, dasie4];
 
 const App = () => {
-  const [imageSource, setImageSource] = useState(DEMO_IMAGE);
+  const [imageIndex, setImageIndex] = useState(0);
+  const [imageSource, setImageSource] = useState(DASIE_IMAGES[0]);
   const [options, setOptions] = useState<PuzzleOptions | undefined>(undefined);
   const [puzzleKey, setPuzzleKey] = useState(0);
 
   const handleRefresh = () => {
-    // TODO: use a random image generator
-    setImageSource(DEMO_IMAGE_2);
+    // ycle through images
+    const nextIndex = (imageIndex + 1) % DASIE_IMAGES.length;
+    setImageIndex(nextIndex);
+    setImageSource(DASIE_IMAGES[nextIndex]);
   };
 
   const handlePropsChange = (newOptions: PuzzleOptions) => {
